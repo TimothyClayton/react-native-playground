98d764dc252785daa5410e842a49a039473c365b
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2018-12-11 12:09:41 -0700

    First commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import AppReducer from './src/reducers/AppReducer';
+import AppWithNavigationState from './src/components/AppNavigator';
+
+class AwesomeProjectApp extends React.Component {
+  store = createStore(AppReducer);
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProjectApp);
+
+export default AwesomeProjectApp;
