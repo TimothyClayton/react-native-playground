9ed8af5f945e5dc6d3626227530335b8e5c3f7ba
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2018-12-12 19:13:05 -0700

    Progress on Enzyme usage in specs

--- src/components/_tests_/chooseColorPage.spec.js
+++ src/components/_tests_/chooseColorPage.spec.js
@@ -1,3 +1,25 @@
 import React from 'react'
+import { Button } from 'react-native'
 import ChooseColorPage from '../ChooseColorPage'
-import TestRenderer from 'react-test-renderer'
+import { shallow, simulate } from 'enzyme'
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Colors from '../../state/Colors';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('Renders correctly', () => {
+  const wrapper = shallow(
+    <Button key={1} title={'BLUE'} color={'str'} onPress={ jest.fn() } />
+    // <ChooseColorPage colorChanged={ jest.fn() } navigation={{ goBack: jest.fn() }} />
+  )
+  // wrapper.find(Button).simulate('click')
+  expect(wrapper).toMatchSnapshot();
+})
+
+it('shows all the colors', () => {
+  const wrapper = shallow(<ChooseColorPage />)
+  console.log(wrapper)
+  // expect()
+})
