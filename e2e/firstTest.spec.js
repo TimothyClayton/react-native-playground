91a832f623a3048d3d01b18e6cbf924421647e45
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2018-12-13 18:33:20 -0700

    More detox tests

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,19 +1,22 @@
 import { takeScreenshot } from './helpers';
 
-describe('Example', () => {
+describe('Color Selection', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
   it('should have a choose color button', async () => {
+    // it('should have a choose color button', () => {
     await expect(element(by.id('color-options-button'))).toBeVisible();
     await expect(element(by.id('main-view'))).toBeVisible();
   });
 
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it('should show choose color screen after tap', async () => {
+    await element(by.id('color-options-button')).tap();
+    await expect(element(by.text('RED'))).toBeVisible();
+    await expect(element(by.text('GREEN'))).toBeVisible();
+    await expect(element(by.text('BLUE'))).toBeVisible();
+  });
 
   // it('should show world screen after tap', async () => {
   //   await element(by.id('world_button')).tap();
